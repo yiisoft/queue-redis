e34aba693b1910cd85a0f09012c8d412489cd08b
aegorov <anat-gor@ya.ru>
2024-04-03 20:03:26 +0300

    fix Fatal error: Uncaught ErrorException: Duplicate key "Yiisoft\Queue\Cli\LoopInterface"

--- config/di.php
+++ config/di.php
@@ -2,7 +2,5 @@
 declare(strict_types=1);
 
 return [
-    \Yiisoft\Queue\Message\MessageSerializerInterface::class => \Yiisoft\Queue\Message\JsonMessageSerializer::class,
     \Yiisoft\Queue\Redis\QueueProviderInterface::class => \Yiisoft\Queue\Redis\QueueProvider::class,
-    \Yiisoft\Queue\Cli\LoopInterface::class => \Yiisoft\Queue\Cli\SignalLoop::class
 ];
